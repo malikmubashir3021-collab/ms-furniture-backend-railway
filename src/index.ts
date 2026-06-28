7b8fc7c6b8119950a6a6929bae0a74e81dbbf855
Malik Mubashir <malikmubashir3021@users.noreply.github.com>
2026-06-28 18:54:34 +0500

    Add /images/ static route on Railway, copy product images from store

--- src/index.ts
+++ src/index.ts
@@ -20,6 +20,7 @@ app.use(cors({
 }))
 
 app.use(express.json())
+app.use('/images', express.static(path.join(__dirname, '..', 'public', 'images')))
 app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')))
 app.use('/admin', express.static(path.join(__dirname, '..', 'admin', 'dist')))
 
